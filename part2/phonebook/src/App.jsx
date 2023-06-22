b8edf55576d3afbf10fa25cbb6d63f5d875cd2c4
TheoSludge <amarko.c@hotmail.com>
2023-06-22 17:24:41 +0200

    phonebook 2.11 done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,20 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import NumbersList from './components/NumbersList'
 import Search from './components/Search'
 import AddContact from './components/AddContact'
 
 const App = () => {
-  const [persons, setPersons] = useState([ // Values for testing
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
 
-  // States
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterValue, setFilterValue] = useState('')
+
+  // Get request for fetching person info from the server
+  const hook = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }
+
+  // Effect hook
+  useEffect(hook, [])
 
   // Info typing handlers
   const handleName = (event) => setNewName(event.target.value)
